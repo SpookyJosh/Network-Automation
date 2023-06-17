57236250288ba1f28e44cc7c39a253ea22b9ef77
Josh Smith <jsmith.wooster@gmail.com>
2023-06-16 20:19:25 -0400

    some json stuff

---
+++ dict-to-json.py
@@ -0,0 +1,6 @@
+import json
+
+mydict = {"key1":"value1","key2":"value2","key3":[{"Property1":"sub-property1"}]}
+
+jsondata = json.dumps(mydict)
+
---
+++ json-test.py
@@ -0,0 +1,30 @@
+{
+    "hostname":"cisco-sjc-rtr-01",
+    "os_version":"16.09",
+    "vlans_list":[
+        {
+            "id": "100"
+            "name": "server_vlan"
+        },
+        {
+            "id": "200"
+            "name": "server_vlan"
+        },        
+        {
+            "id": "300"
+            "name": "video_vlan"
+        }
+    ],
+    "snmp": {
+        "ro": [
+            "public",
+            "cisco"
+        ],
+        "rw": [
+            "private",
+            "secure"
+        ]
+    },
+    "contact":"Cisco Student",
+    "location": "San Jose"
+}
